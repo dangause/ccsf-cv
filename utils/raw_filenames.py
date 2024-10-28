b7f980d35a8f42fe3bbe64d61bdaed8aac7c9922
danpgause <danpgause@gmail.com>
2024-10-28 11:41:21 -0700

    org updates

---
+++ utils/raw_filenames.py
@@ -0,0 +1,60 @@
+
+
+filename_d = {
+    'target': {
+        'ay_psc': [
+            'd4082.fits',
+            'd4083.fits',
+            'd4084.fits',
+            'd4085.fits',
+            'd4086.fits'
+            ]
+    },
+    'bias': [
+        'd4000.fits',
+        'd4001.fits',
+        'd4002.fits',
+        'd4003.fits',
+        'd4004.fits',
+        'd4005.fits',
+        'd4006.fits',
+        'd4007.fits',
+        'd4008.fits',
+        'd4009.fits',
+        'd4010.fits',
+        'd4011.fits',
+        'd4012.fits',
+        'd4013.fits',
+        'd4014.fits'
+       ],
+    'flat': {
+        'dome_30': [
+            'd4015.fits',
+            'd4016.fits',
+            'd4017.fits',
+            'd4018.fits',
+            'd4019.fits'
+            ],
+        'dome_5': [
+            'd4025.fits',
+            'd4026.fits',
+            'd4027.fits',
+            'd4028.fits',
+            'd4029.fits'
+            ],
+        'dome_4': [
+            'd4020.fits',
+            'd4021.fits',
+            'd4022.fits',
+            'd4023.fits',
+            'd4024.fits'
+            ],
+        'dome_3p5': [
+            'd4030.fits',
+            'd4031.fits',
+            'd4032.fits',
+            'd4033.fits',
+            'd4034.fits'
+            ]
+    }
+}
